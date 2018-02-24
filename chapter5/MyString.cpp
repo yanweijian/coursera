350a316f4504b3b8713d4ba7156e7733b1970d75
yanweijian <yan_weijian@outlook.com>
2018-02-24 21:59:14 +0800

    chapter5

---
+++ chapter5/MyString.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+    > File Name: MyString.cpp
+    > Author: ma6174
+    > Mail: ma6174@163.com 
+    > Created Time: Fri 16 Feb 2018 08:07:48 PM DST
+ ************************************************************************/
+
+#include<iostream>
+#include<cstring>
+#include<cstdlib>
+
+using namespace std;
+
+int CompareString
